bfe11bf06ea789c1ff71fd750b318aea9a4e9b57
Antonio Villagra De La Cruz <5470315+AntonioVdlC@users.noreply.github.com>
2021-06-20 19:32:52 +0200

    Implement library

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-async function awaitFor() {
-  return null;
+async function awaitFor<T, U = Error>(
+  fn: Promise<T> | Function | Array<Promise<T> | Function>
+): Promise<[T, null] | [null, U]> {
+  try {
+    if (fn instanceof Error) {
+      // We throw right away if the input is an Error
+      throw fn;
+    }
+
+    let result;
+
+    if (fn instanceof Promise) {
+      // Input is a Promise, we need to await
+      result = await fn;
+    } else if (fn instanceof Function) {
+      // Input is a Function, we need to call it
+      // Note that we can `await` for both sync and async Functions alike
+      result = await fn();
+    } else if (fn instanceof Array) {
+      // Input is an Array, we need to go over each element
+      // Note that we use `Promise.all` so that we can reject as soon as an input throws or is rejected
+      result = await Promise.all(
+        fn.map((f) => {
+          if (f instanceof Error) {
+            // Element is an Error, throw
+            throw f;
+          }
+
+          if (f instanceof Function) {
+            // Element is a Function, call
+            return f();
+          }
+
+          // Element is anything else, return
+          // Note that if this was an Array, we would not recursively go over all
+          // its elements at a further depth
+          return f;
+        })
+      );
+    } else {
+      // Element is anything else, return value
+      result = fn;
+    }
+
+    return [result, null];
+  } catch (error) {
+    return [null, error];
+  }
 }
 
 export default awaitFor;
--- test/index.test.js
+++ test/index.test.js
@@ -4,55 +4,120 @@ describe("await-for", () => {
   it("is a function", () => {
     expect(typeof awaitFor).toBe("function");
   });
+
   it("returns an Array containing 2 values", async () => {
     const result = await awaitFor(Promise.resolve(42));
 
     expect(Array.isArray(result)).toBe(true);
     expect(result.length).toBe(2);
   });
+
   it("returns a non-Error value as the first element of the Array and a null value as the second element if the Promise resolves", async () => {
     const [result, error] = await awaitFor(Promise.resolve(42));
 
     expect(result).not.toBeNull();
     expect(error).toBeNull();
   });
+
   it("returns a null value as the first element of the Array and an Error as the second element if the Promise rejects", async () => {
     const [result, error] = await awaitFor(Promise.reject("Some error"));
 
     expect(result).toBeNull();
     expect(error).not.toBeNull();
   });
+
   it("returns a null value as the first element of the Array and an Error as the second element if the Promise throws", async () => {
+    const someError = new Error("Some error");
     const promise = new Promise(() => {
-      throw new Error("Some error");
+      throw someError;
     });
+
     const [result, error] = await awaitFor(promise);
 
     expect(result).toBeNull();
-    expect(error).not.toBeNull();
+    expect(error).toEqual(someError);
+  });
+
+  it("returns a null value as the first element of the Array and an Error as the second element if at least one Promise throws", async () => {
+    const someError = new Error("Some error");
+
+    const promise1 = new Promise(() => {
+      throw someError;
+    });
+    const promise2 = new Promise((resolve) => resolve(42));
+
+    const [result, error] = await awaitFor([promise1, promise2]);
+
+    expect(result).toBeNull();
+    expect(error).toEqual(someError);
+  });
+
+  it("returns a null value as the first element of the Array and an Error as the second element if at least one of the inputs is an Error", async () => {
+    const someError = new Error("Some error");
+    const promise = new Promise((resolve) => resolve(42));
+    const fn = () => 42;
+
+    const [result, error] = await awaitFor([promise, someError, fn]);
+
+    expect(result).toBeNull();
+    expect(error).toEqual(someError);
+  });
+
+  it("returns a null value as the first element of the Array and an Error as the second element if passed an error", async () => {
+    const someError = new Error("Some error");
+
+    const [result, error] = await awaitFor(someError);
+
+    expect(result).toBeNull();
+    expect(error).toEqual(someError);
   });
 
   it("accepts a value as argument", async () => {
     const value = 42;
+
     const [result, error] = await awaitFor(value);
 
     expect(result).toEqual(value);
     expect(error).toBeNull();
   });
+
   it("accepts a Promise as argument", async () => {
     const value = 42;
     const promise = new Promise((resolve) => resolve(value));
+
     const [result, error] = await awaitFor(promise);
 
     expect(result).toEqual(value);
     expect(error).toBeNull();
   });
-  it("accepts an Array of Promises as argument", async () => {
+
+  it("accepts a Function as argument", async () => {
+    const value = 42;
+    const fn = () => value;
+
+    const [result, error] = await awaitFor(fn);
+
+    expect(result).toEqual(value);
+    expect(error).toBeNull();
+  });
+
+  it("accepts an async Function as argument", async () => {
+    const value = 42;
+    const fn = async () => new Promise((resolve) => resolve(value));
+
+    const [result, error] = await awaitFor(fn);
+
+    expect(result).toEqual(value);
+    expect(error).toBeNull();
+  });
+
+  it("accepts an Array of Promises and Functions as argument", async () => {
     const value1 = "hello";
     const value2 = "world";
-    const promise1 = new Promise((resolve) => resolve(value1));
-    const promise2 = new Promise((resolve) => resolve(value2));
-    const [result, error] = await awaitFor([promise1, promise2]);
+    const promise = new Promise((resolve) => resolve(value1));
+    const fn = () => new Promise((resolve) => resolve(value2));
+
+    const [result, error] = await awaitFor([promise, fn]);
 
     expect(Array.isArray(result)).toBe(true);
     expect(result[0]).toEqual(value1);
